b07c5622c890782e18dfc210b043528b4bfcd9c4
tigermask1978 <tigermask1978@gmail.com>
2022-10-08 10:33:18 +0800

    add static code check

--- src/main/java/com/tigermask/calculator/CalculatorApplication.java
+++ src/main/java/com/tigermask/calculator/CalculatorApplication.java
@@ -3,6 +3,10 @@ package com.tigermask.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+* Main Spring Application.
+*/
 @SpringBootApplication
 public class CalculatorApplication {
 
--- src/main/java/com/tigermask/calculator/ServletInitializer.java
+++ src/main/java/com/tigermask/calculator/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.tigermask.calculator;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * ServletInitializer
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
